70c33237206eb5f452551dd83d12dfc0dffbd32a
rithiksabhal <coderithik@gmail.com>
2023-04-07 17:23:39 +0530

    Added initial gateway files

---
+++ src/main/java/com/glimmertech/bagateway/BaGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.glimmertech.bagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaGatewayApplication.class, args);
+	}
+
+}
